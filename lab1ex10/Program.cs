0f2cf45f24c2babc1fbab166b4bef56e04317805
victor.gozu <victor.gozu@MP150159>
2024-02-19 19:28:46 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Scrieti un program care va afisa suma cifrelor unui numar
+* intreg citit de la tastatura
+*/
+
+Console.WriteLine("Introduceti un numar intreg cu 2 cifre:");
+int numar = int.Parse(Console.ReadLine());
+
+int cifraUnu, cifraDoi, sum=0;
+
+if
+    (numar < 100)
+{
+    if (numar > 9)
+    {
+        cifraDoi = numar % 10;
+        cifraUnu = numar / 10;
+        sum = cifraUnu + cifraDoi;
+        Console.WriteLine("Suma cifrelor este:" + sum);
+    }
+    else 
+    {
+        Console.WriteLine("Numarul are doar 1 cifra");
+    }
+
+}
+else
+{
+    Console.WriteLine("Numarul nu are 2 cifre");
+}
